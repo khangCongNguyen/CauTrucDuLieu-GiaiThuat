cf9dcdf03ccffc2d562633f722897a516965fafe
khangCongNguyen <anhbinhcachu@gmail.com>
2021-10-03 15:33:13 +0700

    SinhVien

---
+++ src/com/khangnc2008110130/tuan02/SinhVien.java
@@ -0,0 +1,18 @@
+package com.khangnc2008110130.tuan02;
+
+public class SinhVien {
+    String HovaTen;
+    int namSinh;
+    String MSSV;
+    int tuoi;
+    String queQuan;
+    String lop;
+    void inThongTinSinhVien(){
+        System.out.println("mã số sinh viên    :"+MSSV);
+        System.out.println("lớp                :"+lop);
+        System.out.println("họ và tên sinh viên:"+HovaTen);
+        System.out.println("năm sinh           :"+namSinh);
+        System.out.println("tuổi               :"+tuoi);
+        
+    }
+}
---
+++ src/com/khangnc2008110130/tuan02/SinhVienTestDrive.java
@@ -0,0 +1,15 @@
+package com.khangnc2008110130.tuan02;
+
+public class SinhVienTestDrive {
+    public static void main(String[] args) {
+        SinhVien sinhVien = new SinhVien();
+        sinhVien.HovaTen=("NGUYỄN CÔNG KHANG");
+        sinhVien.MSSV=("2008110130");
+        sinhVien.lop=("K14DCPM01");
+        sinhVien.namSinh=2002;
+        sinhVien.queQuan=(" Sông Luỹ - Bắc Bình -Bình thuận");
+        sinhVien.tuoi=20;
+        sinhVien.inThongTinSinhVien();
+    }
+    
+}
