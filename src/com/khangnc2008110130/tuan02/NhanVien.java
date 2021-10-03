788dbd2d9d85ab2d0ad931799e4b44e815b11712
khangCongNguyen <anhbinhcachu@gmail.com>
2021-10-03 16:50:35 +0700

    Bài tập thêm NhanVien

---
+++ src/com/khangnc2008110130/tuan02/NhanVien.java
@@ -0,0 +1,16 @@
+package com.khangnc2008110130.tuan02;
+
+public class NhanVien {
+    String tenNhanVien;
+    String luong;
+    String diaChi;
+    String boPhanLamViec;
+    String ngaySinh;
+    void inThongTinNhanVien(){
+        System.out.println("tên nhân viên   :"+tenNhanVien);
+        System.out.println("lương           :"+luong);
+        System.out.println("địa chỉ         :"+diaChi);
+        System.out.println("bộ phận làm việc:"+boPhanLamViec);
+        System.out.println("ngày sinh       :"+ngaySinh);
+    }
+}
---
+++ src/com/khangnc2008110130/tuan02/NhanVienTestDrive.java
@@ -0,0 +1,14 @@
+package com.khangnc2008110130.tuan02;
+
+public class NhanVienTestDrive {
+    public static void main(String[] args) {
+        NhanVien nhanVien=new NhanVien();
+        nhanVien.boPhanLamViec=("phục vụ  trong quán ăn ");
+        nhanVien.diaChi=("9 Nguyễn Trọng Tuyển ");
+        nhanVien.luong=("1h ->20k");
+        nhanVien.tenNhanVien=(" Nguyễn Công Khang");
+        nhanVien.ngaySinh=("10/05/2002");
+        nhanVien.inThongTinNhanVien();
+    }
+    
+}
