da276eca5458679dd3450f9c9091ed4c173149ee
khangCongNguyen <anhbinhcachu@gmail.com>
2021-10-03 16:47:31 +0700

    Bai tập thêm Sách

---
+++ src/com/khangnc2008110130/tuan02/Sach.java
@@ -0,0 +1,18 @@
+package com.khangnc2008110130.tuan02;
+
+public class Sach {
+    double gia;
+    String nhaXuatBan;
+    String namXuatBan;
+    double giaBan;
+    int soLuong;
+    String loai;
+    void inThongTinSach(){
+        System.out.println("giá            :"+gia);
+        System.out.println("nhà sản xuất   :"+nhaXuatBan);
+        System.out.println("năm xuất bản   :"+namXuatBan);
+        System.out.println("giá bán        :"+giaBan);
+        System.out.println("số lượng       :"+soLuong);
+        System.out.println("loại           :"+loai);
+    }
+}
---
+++ src/com/khangnc2008110130/tuan02/SachTestDrive.java
@@ -0,0 +1,15 @@
+package com.khangnc2008110130.tuan02;
+
+public class SachTestDrive {
+    public static void main(String[] args) {
+        Sach sach =new Sach();
+        sach.gia=12.5;
+        sach.giaBan=100;
+        sach.loai=("Sách Lập Trình");
+        sach.namXuatBan=("2021");
+        sach.nhaXuatBan=("những lập trình viên ");
+        sach.soLuong=1;
+        sach.inThongTinSach();
+    }
+    
+}
