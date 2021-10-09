c89dca8822ffb70a08a4fdc23b38335c63e016da
khangCongNguyen <anhbinhcachu@gmail.com>
2021-10-10 00:44:10 +0700

    Bài tập 2 lab 3

---
+++ src/com/khangnc2008110130/tuan03/Bai2Lab3.java
@@ -0,0 +1,16 @@
+package com.khangnc2008110130.tuan03;
+
+public class Bai2Lab3 {
+    public static void main(String[] args) {
+        // in bảng cửu chưởng
+        System.out.println("bảng cửu chương từ 1 đến 10");
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                System.out.println(i+"x"+j+"="+i*j);
+            }
+          System.out.println(""); 
+
+    }
+    }
+    
+}
